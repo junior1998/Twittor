aaa196505b8e0b35ebd2890f24487f40e686d186
Yunior <juniorliban@hotmail.com>
2018-10-20 14:22:11 -0400

    CAmbios para iphones

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // Imports 
 importScripts('js/sw-autils.js');
 
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v4';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 
@@ -51,6 +51,11 @@ self.addEventListener('activate', e => {
                 if (key !== STATIC_CACHE && key.includes('static')) {
                     return caches.delete(key);
                 }
+
+                if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
+                    return caches.delete(key);
+                }
+
             });
         });
 
